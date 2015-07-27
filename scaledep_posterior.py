c08cb80baf0a6fd7f178ebe989f0f4ae420cdcff
Saroj Adhikari <adh.saroj@psu.edu>
2015-07-26 20:19:41 -0400

    posterior2d dist of fNL0, nfNL by combining (lmin, lmax) likelihoods

--- scaledep_posterior.py
+++ scaledep_posterior.py
@@ -1,4 +1,6 @@
 from posterior import PosteriorfNL
+import numpy as np
+from scipy import integrate
 
 pf=PosteriorfNL()
 
@@ -14,13 +16,20 @@ class PosteriorSDfNL(PosteriorfNL):
         self.A1const=0.0258/500. # for ns=0.965
     
     def fNLl(self, l, fNL60=50., nfNL=-1.0):
-        return fNL0*(l/self.l0)**nfNL
+        return fNL60*(l/self.l0)**nfNL
     
     def pdfl(self, l, A, nA, fNL60, nfNL):
         self.sigmaG=2.84/(2.*l+1)
-        return self.pdf(self.fNLl(l, A, nA), self.fNLl(l, fNL0, nfNL))
+        return self.pdf(self.fNLl(l, A, nA), self.fNLl(l, fNL60, nfNL))
     
-    def pdf_multipoles(self, lmin, lmax, A60, nA):
+    def pdf_multipoles(self, lmin, lmax, A60, nA, f60, nf):
+        pdfm=1.
+        for l in np.arange(lmin, lmax+1):
+            pdfm=pdfm*self.pdfl(l, A60, nA, f60, nf)
+        
+        return pdfm
+    
+    def post_multipoles(self, lmin, lmax, A60, nA):
         """
         use the combined likelihood of different multipoles given a measurement of
         (A, nA) for fNL0 and nfNL, where 
@@ -29,5 +38,20 @@ class PosteriorSDfNL(PosteriorfNL):
         
         to get a 2D posteriro pdf in the plane (fNL, nfNL)
         """
+        lmodes=lmax-lmin+1
+        f60list=np.arange(0.1, 1000., 2.)
+        nfNLlist=np.arange(-2.0, 1.0, 0.01)
+        posterior2d=np.ones((len(f60list), len(nfNLlist)))
+        print np.shape(posterior2d)
+        
+        post = lambda fNL,nfNL: self.pdf_multipoles(lmin, lmax, A60, nA, fNL, nfNL)
+        norm2d = integrate.nquad(post, [[0.0, 4000.],[-5.0, 5.0]])[0]
+        print norm2d
+        
+        for f in range(len(f60list)):
+            for n in range(len(nfNLlist)):
+                posterior2d[f][n]=post(f60list[f], nfNLlist[n])
+
+        return posterior2d/norm2d
         
         
